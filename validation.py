fdcba6595fd4f1928190224143ac7849b8961071
Oliver Bulman <138403862+ojbulman@users.noreply.github.com>
2024-03-01 15:54:48 +0000

    Removed re package dependency

--- validation.py
+++ validation.py
@@ -1,7 +1,3 @@
-import re
-from six import string_types
-
-
 def boolean_str(val, allow_empty=False):
     if allow_empty and (val is None): val = ""
     if not isinstance(val, str): return False
@@ -57,24 +53,26 @@ def email_address(val, allow_empty=False):
     return True
 
 
-def password(val, min_length=10, req_numbers=True, req_mix_case=True, req_special_chars=True, accepted_special_chars='[@_-!£$%^&*<>?/\|}{~:]#'):
+def password(val, min_length=10, req_numbers=True, req_mix_case=True, req_special_chars=True, accepted_special_chars='-[@_!£$%^&*<>?/\|}{~:]#'):
     if not isinstance(val, str):        return False
     if len(val.strip()) < min_length:   return False
     
     if req_numbers:
         nums = [x for x in val if x.isdigit()]
+
         if not nums:
             return False
     
     if req_mix_case:
         lcase = [x for x in val if x.islower()]
         ucase = [x for x in val if x.isupper()]
+
         if not all([lcase,ucase]):
             return False
 
     if req_special_chars:
-        special_chars = re.compile(accepted_special_chars)
-        if special_chars.search(val) == None:
+        special_chars = [x for x in val if x in accepted_special_chars]
+        if not special_chars:
             return False
     
     return True
